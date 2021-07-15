55da8790c245b2075d5a044bd78000cfa465957a
Svitlana-G <svitlana.golubieva@gmail.com>
2021-07-15 12:23:24 +0200

    Added articles with photos

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
+
+const data = require('./website.json')
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', { data })
+})
+
+app.get('/article', (req, res) => {
+    res.render('article')
+})
+
+app.get('/new', (req, res) => {
+    res.render('new')
 })
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`))

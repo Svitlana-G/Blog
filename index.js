a2684349b72cfca4df3b34ade55e3254261f5709
Svitlana-G <svitlana.golubieva@gmail.com>
2021-07-16 15:26:19 +0200

    Refine style the pages

--- index.js
+++ index.js
@@ -4,52 +4,48 @@ const app = express()
 const PORT = process.env.PORT || 3060
 
 const data = require('./website.json')
-const { url } = require('inspector')
-const { title } = require('process')
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
-// app.use(express.json())
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.render('index', { data })
+    res.render('index', { data, menu: "All" })
 })
 
 app.post('/add', (req, res) => {
     // console.log(req)
     console.log(req.body)
     idCurrent = data.length
-    console.log(url)
     data.push({
         id: idCurrent,
         url: req.body.url,
         title: req.body.myTitle,
         body: req.body.body,
-        // published_at: title,
-        // dduration: title,
         author: req.body.author,
         author_bild: req.body.author_bild
     })
-    console.log(data)
+    // console.log(data)
     fs.writeFile('./website.json', JSON.stringify(data), 'utf-8', (err) => {
         if (err) throw err
     })
-    res.redirect('/')
+    res.redirect('/newArticle')
 })
 
 
 app.get('/newArticle', (req, res) => {
-    res.render('newArticle', { data })
+    res.render('newArticle', { data, menu: "All" })
 })
 
 
-app.get('/:article', (req, res) => {
+app.get('/article/:article', (req, res) => {
     console.log(req.params.article)
     let currentArticle = data.filter(ele => ele.id == req.params.article)
     console.log(currentArticle)
-    res.render('article', { article: currentArticle[0], data })
+    res.render('article', { article: currentArticle[0], data, menu: "Blog" })
+
 })
 
 
